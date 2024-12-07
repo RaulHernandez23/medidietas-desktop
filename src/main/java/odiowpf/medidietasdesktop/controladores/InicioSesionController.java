59cdc7a792f6a7b169c64799cde57c3f98f3fe7e
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-12-07 17:59:44 -0600

    Preparación de merge

--- src/main/java/odiowpf/medidietasdesktop/controladores/InicioSesionController.java
+++ src/main/java/odiowpf/medidietasdesktop/controladores/InicioSesionController.java
@@ -12,9 +12,6 @@ import odiowpf.medidietasdesktop.daos.ExpertoNutricionDAO;
 import odiowpf.medidietasdesktop.modelos.ExpertoNutricion;
 import odiowpf.medidietasdesktop.utilidades.Constantes;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class InicioSesionController {
@@ -25,21 +22,22 @@ public class InicioSesionController {
     @FXML
     public void btnEntrar(ActionEvent actionEvent) {
         try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/odiowpf/medidietasdesktop/vistas/FXMLMenu.fxml"));
-//            Scene scene = new Scene(loader.load(), 1280, 720);
-//
-//            Stage stage = new Stage();
-//            stage.setTitle("Menú principal");
-//            stage.setResizable(false);
-//            stage.setScene(scene);
-//            stage.show();
-//
-//            Stage stageActual = (Stage) paneRaiz.getScene().getWindow();
-//            stageActual.close();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/odiowpf/medidietasdesktop/vistas/FXMLMenu.fxml"));
+            Scene scene = new Scene(loader.load(), 1280, 720);
+
+            Stage stage = new Stage();
+            stage.setTitle("Menú principal");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+            Stage stageActual = (Stage) paneRaiz.getScene().getWindow();
+            stageActual.close();
             HashMap<String, Object> respuesta = ExpertoNutricionDAO.logIn("lgomez@ejemplo.com", "contrasena123");
 
             if ((boolean) respuesta.get(Constantes.KEY_ERROR)) {
                 ExpertoNutricion experto = (ExpertoNutricion) respuesta.get(Constantes.KEY_OBJETO);
+                System.out.println(experto.getNombre());
             }
         } catch (Exception ex) {
             ex.printStackTrace();
