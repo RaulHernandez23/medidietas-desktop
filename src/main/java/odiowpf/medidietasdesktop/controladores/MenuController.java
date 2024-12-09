c64366aedc774e315c209b9d129605949af4f7f0
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-12-09 14:07:02 -0600

    Se agregaron las primeras validaciones de datos a RegistrarComida

--- src/main/java/odiowpf/medidietasdesktop/controladores/MenuController.java
+++ src/main/java/odiowpf/medidietasdesktop/controladores/MenuController.java
@@ -23,19 +23,12 @@ import java.util.HashMap;
 
 public class MenuController
 {
-
     private ExpertoNutricion experto;
     private Image fotoPerfil;
     @javafx.fxml.FXML
     private AnchorPane paneRaiz;
     @javafx.fxml.FXML
-    private VBox barraNavegacion;
-    @javafx.fxml.FXML
-    private StackPane contenedorLogo;
-    @javafx.fxml.FXML
     private StackPane paneContenido;
-    @javafx.fxml.FXML
-    private MFXButton btnSalir;
     @javafx.fxml.FXML
     private MFXButton btnAlimentos;
     @javafx.fxml.FXML
