29be6f1fb68c38fbcfe3d0b7a06ec43a20e371e7
Raul <raulh230600@gmail.com>
2024-12-08 18:36:10 -0600

    feat(Iniciar sesion): agregada funcionalidad al inicio de sesion

--- src/main/java/odiowpf/medidietasdesktop/utilidades/GestorToken.java
+++ src/main/java/odiowpf/medidietasdesktop/utilidades/GestorToken.java
@@ -2,7 +2,6 @@ package odiowpf.medidietasdesktop.utilidades;
 
 public class GestorToken {
 
-    public static String TOKEN
-            = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjMsImlhdCI6MTczMzY4ODczMSwiZXhwIjoxNzMzNjk1OTMxfQ.-jZkHvfouini5xt4h-V0EcdY9ugSfwBuHwRdJM35mpI";
+    public static String TOKEN = "";
 
 }
